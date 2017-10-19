a1fcb6245e930a49599f08719bdf83789ce6667d
Cofess <285088180@qq.com>
2017-10-19 09:37:39 +0800

    no message

--- gulp/tasks/development/jsconcat.js
+++ gulp/tasks/development/jsconcat.js
@@ -3,22 +3,23 @@ var folders         = require('gulp-recursive-folder')
 var gulp            = require('gulp')
 var concat          = require('gulp-concat')
 var sourcemap       = require('gulp-sourcemaps')
-var config          = require('../../config').js.concat
+var gulpif          = require('gulp-if')
+var config          = require('../../config').js
 
 
 if (!config) return
 
 gulp.task('jsconcat', folders({
-  base: config.folder,
-  exclude: config.excludeFolders,
+  base: config.concat.folder,
+  exclude: config.concat.excludeFolders,
 }, function(folder) {
   //This will loop over all folders inside pathToFolder main and recursively on the children folders, secondary 
   //With folderFound.name gets the folderName 
   //With folderFound.path gets all folder path found 
   //With folderFound.pathTarget gets the relative path beginning from options.pathFolder 
   return gulp.src([folder.path + "/**/*.js","!" + folder.path + "/_*/*.js","!" + folder.path + "/**/_*.js"])
-    .pipe(sourcemap.init())
+    .pipe(gulpif(config.sourcemap,sourcemap.init()))
     .pipe(concat(folder.name + ".js"))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest(config.dest));
+    .pipe(gulpif(config.sourcemap && '!*min.js',sourcemap.write('./')))
+    .pipe(gulp.dest(config.concat.dest));
 }));
